6fa822ceb6d0015ec7ccfa308c514097ce7bc150
HanSquared <42755592+HanSquared@users.noreply.github.com>
2024-06-02 14:57:00 -0400

    generate raw dataset data report

--- Code/Data_Acquisition_and_Understanding/dataPrep.py
+++ Code/Data_Acquisition_and_Understanding/dataPrep.py
@@ -6,4 +6,11 @@ import pandas as pd
 dataLoc ="/Users/hanhan/Desktop/CMU/Software_Design_for_Data_Scientists/GitHub/cmu-95829-Azure-TDSP/Sample_Data/Raw/WA_Fn-UseC_-Telco-Customer-Churn.csv"
 df = pd.read_csv(dataLoc,sep = ',')
 # Display the first few rows of the dataframe to ensure it's loaded correctly
+print("Data preview:")
 print(df.head())
+print("\nData info:")
+print(df.info)
+print("\nData description:")
+print(df.describe())
+
+
