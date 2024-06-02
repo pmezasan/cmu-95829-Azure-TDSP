daacdbb69c6e43d6e087ddb2a8e0997e34eb5f7a
HanSquared <42755592+HanSquared@users.noreply.github.com>
2024-06-02 14:01:06 -0400

    Update dataPrep.py

--- Code/Data_Acquisition_and_Understanding/dataPrep.py
+++ Code/Data_Acquisition_and_Understanding/dataPrep.py
@@ -1,7 +1,7 @@
 #Load the CSV File
 import pandas as pd
 
-Load the CSV file using pandas
+#Load the CSV file using pandas
 url = 'https://github.com/HanSquared/cmu-95829-Azure-TDSP/blob/main/Sample_Data/Raw/WA_Fn-UseC_-Telco-Customer-Churn.csv' 
 data = pd.read_csv(url)
 # Display the first few rows of the dataframe to ensure it's loaded correctly
