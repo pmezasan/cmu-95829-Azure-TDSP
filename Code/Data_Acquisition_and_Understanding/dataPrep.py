a73810bba4406e8805b06e2a16d802b9cfd02e46
HanSquared <42755592+HanSquared@users.noreply.github.com>
2024-06-02 14:13:01 -0400

    Update dataPrep.py

--- Code/Data_Acquisition_and_Understanding/dataPrep.py
+++ Code/Data_Acquisition_and_Understanding/dataPrep.py
@@ -2,7 +2,8 @@
 import pandas as pd
 
 #Load the CSV file using pandas
-url = 'https://github.com/HanSquared/cmu-95829-Azure-TDSP/blob/main/Sample_Data/Raw/WA_Fn-UseC_-Telco-Customer-Churn.csv' 
-data = pd.read_csv(url)
+#url = 'https://github.com/HanSquared/cmu-95829-Azure-TDSP/blob/main/Sample_Data/Raw/WA_Fn-UseC_-Telco-Customer-Churn.csv' 
+dataLoc ="/Users/hanhan/Desktop/CMU/Software_Design_for_Data_Scientists/GitHub/cmu-95829-Azure-TDSP/Sample_Data/Raw/WA_Fn-UseC_-Telco-Customer-Churn.csv"
+df = pd.read_csv(dataLoc,sep = ',')
 # Display the first few rows of the dataframe to ensure it's loaded correctly
-print(data.head())
+print(df.head())
